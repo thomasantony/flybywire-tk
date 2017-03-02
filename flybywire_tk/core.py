5da8e2df83edff8a195ff80a670ed55c6ad21101
Thomas Antony <tantony@purdue.edu>
2017-03-02 01:31:07 -0500

    Added component tree parsing with unit tests

--- flybywire_tk/core.py
+++ flybywire_tk/core.py
@@ -2,12 +2,43 @@
 import abc
 import asyncio as aio
 import tkinter
+import inspect
+import collections
+import copy
 from tkinter import N,NE,E,SE,S,SW,W,NW
 from misc import set_interval, clear_interval, AutoScrollbar
+import ui
 
-def T(name, children=None, **kwargs):
+def T(name, content=None, **props):
     """Helper function for building components."""
-    return dict(component=name, children=children, **kwargs)
+    return dict(_name=name, text=content, _props=props)
+
+
+def parse_component_tree(root_node):
+    """
+    Travereses and builds the component tree
+
+    Parameters
+    ----------
+    root_comp : dict
+        Root component description generated using the T() function
+    """
+    output = {}
+    comp_name = root_node['_name']
+    comp_props = root_node['_props']
+
+    if callable(comp_name):
+        comp_node = comp_name(text=root_node['text'], **comp_props)
+    elif comp_name not in ui.available_widgets:
+        raise ValueError('Widget not found : %s' % comp_name)
+    else:
+        comp_node = root_node
+
+    subnodes = comp_node['text']
+    if not isinstance(subnodes, str) and isinstance(subnodes, collections.Iterable):
+        output['text'] = [parse_component_tree(n) for n in subnodes]
+
+    return dict(collections.ChainMap(output, comp_node))
 
 
 class FBWApplication(object):
@@ -18,12 +49,13 @@ class FBWApplication(object):
         self.kw = kw
         self._root_comp = None
 
-    def mount(self, component):
+    def mount(self, component, **props):
         """Mounts the given component in the application."""
         self._root_comp = component
         self._build_app()
 
     def _build_app(self):
+        """Build the tkinter window with default settings."""
         # create scroll bar
         self.vscrollbar = AutoScrollbar(self._root)
         self.vscrollbar.grid(row=0, column=1, sticky=N+S)
@@ -45,6 +77,8 @@ class FBWApplication(object):
         self.frame.columnconfigure(0, weight=1)
         self.frame.columnconfigure(1, weight=1)
 
+        self.draw_component()
+
         # After component creation
         # puts tkinter widget onto canvas
         self.canvas.create_window(0, 0, anchor=NW, window=self.frame, width = int(self.canvas.config()['width'][4])-int(self.vscrollbar.config()['width'][4]))
@@ -52,6 +86,7 @@ class FBWApplication(object):
         # deal with canvas being resized
         def resize_canvas(event):
             self.canvas.create_window(0, 0, anchor=NW, window=self.frame, width = int(event.width)-int(self.vscrollbar.config()['width'][4]))
+
         self.canvas.bind("<Configure>", resize_canvas)
 
         # updates geometry management
@@ -68,8 +103,16 @@ class FBWApplication(object):
         self.is_running = True
         self.frame.update()
 
+    def draw_component(self):
+        comp_tree = self._root_comp()
+        # Recursively build component tree dict
+        # Diff tree against existing tree
+        # Instantiate or modify components as needed (pack(), update() or destroy())
+
+
     @aio.coroutine
     def main_loop(self, loop=aio.get_event_loop()):
+        """Run the tkinter event loop asynchronously."""
         while self.is_running:
             # start mainloop
             self._root.update_idletasks()
@@ -80,6 +123,7 @@ class FBWApplication(object):
                 break
 
     def start(self):
+        """Start the application."""
         loop = aio.get_event_loop()
         def on_closing():
             self.is_running = False
@@ -115,29 +159,47 @@ class TimerApp(Component):
         self.secondsElapsed = 0
         self.task = None
 
-    def __call__(self, props):
+    def __call__(self):
         """Renders view given application state."""
         count = self.secondsElapsed
-        return T('Label', 'Seconds Elapsed: '+str(count))
+        return T(TimerView, self.secondsElapsed)
 
     def tick(self):
         """Increments counter."""
         count = self.secondsElapsed
         self.update(secondsElapsed = count + 1)
 
-    def on_load(self):
+    def on_mount(self):
         """
-        Triggers when the application first loads in the browser
+        Triggers when the component is mounted
         """
         self.task = set_interval(self.tick, 1)
 
-    def on_close(self):
+    def on_unmount(self):
         """
-        Triggers when the application window is closed
+        Triggers when the component is removed
         """
         clear_interval(self.task)
 
+def TimerView(text=0):
+    return T('Label', 'Seconds Elapsed: '+str(text), color='red')
 
-fbw = FBWApplication()
-fbw.mount(TimerApp())
-fbw.start()
+# fbw = FBWApplication()
+# fbw.mount(TimerApp())
+# fbw.start()
+
+if __name__ == '__main__':
+    # Tests for component parsing
+    comps = T('Frame', [T(TimerView, 1337), T(TimerView, 6969)], align='center')
+    out = parse_component_tree(comps)
+    assert out == {'_name': 'Frame',
+             '_props': {'align': 'center'},
+             'text': [{'_name': 'Label',
+                       '_props': {'color': 'red'},
+                       'text': 'Seconds Elapsed: 1337'},
+                      {'_name': 'Label',
+                       '_props': {'color': 'red'},
+                       'text': 'Seconds Elapsed: 6969'}]}
+
+    comps = T(TimerView, 1337)
+    assert parse_component_tree(comps) == {'_name': 'Label', 'text': 'Seconds Elapsed: 1337', '_props': {'color': 'red'}}
--- flybywire_tk/ui.py
+++ flybywire_tk/ui.py
@@ -1,1 +1,26 @@
 """Wrappers for tkinter widgets."""
+import sys
+import inspect
+import tkinter
+
+def Label(self, root, component_def):
+    """A wrapper for tkinter.Label."""
+    _, content, props = component_def
+
+    text_var = tkinter.StringVar()
+    def update(**props):
+        """Function used to update the label if changed."""
+        text_var.set(props['text'] if 'text' in props else content)
+
+    update(**props)
+    if 'text' in props:
+        del props['text']
+
+    return (tkinter.Label(root, textvariable=text_var, **props), update)
+
+def Frame():
+    """Test widget."""
+    pass
+
+available_widgets = {name: fn for name, fn in inspect.getmembers(sys.modules[__name__])
+                     if callable(fn)}
